220649e2d0efb1b8267a4f86cc31f601c3554dd4
mirexdoors <mirex.doors@mail.ru>
2019-09-10 10:51:01 +0700

    add md iconfont

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont';
 
 Vue.use(Vuetify);
 
 new Vue({
   vuetify: new Vuetify({
     theme: {dark: true},
+    icons: {
+      iconfont: 'mdiSvg'
+    }
   }),
   render: h => h(App),
 }).$mount('#app');
