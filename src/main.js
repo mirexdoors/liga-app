81bc7425baad9d55ae088dacdae7cbaede11656e
Mikhail <mirex.doors@mail.ru>
2019-09-13 23:58:27 +0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Vuetify from 'vuetify';
+import Vuex from 'vuex';
 import 'vuetify/dist/vuetify.min.css';
 import 'material-design-icons-iconfont';
 
+Vue.use(Vuex);
 Vue.use(Vuetify);
 
 new Vue({
---
+++ src/store/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    players: null,
+  },
+  getters: {
+    players(state) {
+      return state.players;
+    }
+  },
+  mutations: {
+    setPlayers(state, players) {
+      state.players = players;
+    }
+  },
+  actions: {
+    fetchPlayers({commit}, url) {
+      let result = [];
+      fetch(url, {
+        method: "GET"
+      })
+        .then(response => {
+          return response.json();
+        })
+        .then(responseJSON => {
+           result = responseJSON.players;
+        });
+      if (result) {
+        commit('setPlayers', result);
+      }
+    }
+  }
+});
