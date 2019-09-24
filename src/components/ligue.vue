fa377c3144ff9f8b4f8009cf594f65cfed7d6d24
mirexdoors <mirex.doors@mail.ru>
2019-09-24 12:09:49 +0700

    fix table styles

--- src/components/ligue.vue
+++ src/components/ligue.vue
@@ -1,184 +1,184 @@
 <template>
-  <v-card>
-    <v-card-title>
-      {{currentDate}}
-      <div class="flex-grow-1"></div>
-      <v-text-field v-model="search" append-icon="search" label="Поиск" single-line hide-details></v-text-field>
-    </v-card-title>
-    <v-container>
-      <v-row no-gutters>
-        <v-col v-for="team in teams" v-bind:key="team.title" cols="12" sm="4">
-          <v-card class="ma-1" outlined tile>
-            <v-data-table
-              class="elevation-5"
-              :headers="headers"
-              :items="team.players"
-              :search="search"
-              :items-per-page="30"
-              hide-default-footer
-              dark
-            >
-              <template v-slot:header="{ props: { headers }  }">
-                <thead>
-                  <tr>
-                    <th :colspan="headers.length-1">
-                      <v-chip color="orange" label outlined>{{team.title}}</v-chip>
-                    </th>
-                    <th>
-                      <v-chip color="orange" label outlined>
-                        {{team.total.toFixed(1)}}
-                        <v-icon right>star</v-icon>
-                      </v-chip>
-                    </th>
-                  </tr>
-                </thead>
-              </template>
-              <template v-slot:body.prepend="{headers}">
-                <tr class="header__mobile d-sm-none">
-                  <th
-                    :class="getClassForCol(header.value)"
-                    v-for="header  in headers"
-                    v-bind:key="header.text"
-                  >{{header.text}}</th>
-                </tr>
-              </template>
-              <template v-slot:item="{item}">
-                <tr :style=" getColor(item.division)">
-                  <td :class="getClassForCol('name')">
-                    <router-link class="player__link" :to="translit(item.name)">{{item.name}}</router-link>
-                  </td>
-                  <td class="player__games">
-                    <router-link class="player__link" :to="translit(item.name)">
-                      {{item.games
-                      ? item.games : 0 }}
-                    </router-link>
-                  </td>
-                  <td class="player__games">
-                    <router-link class="player__link" :to="translit(item.name)">
-                      {{item.unique_games ?
-                      item.unique_games : 0}}
-                    </router-link>
-                  </td>
-                  <td class="player__points">
-                    <router-link class="player__link" :to="translit(item.name)">
-                      {{item.points ?
-                      item.points : 0}}
-                    </router-link>
-                  </td>
-                </tr>
-              </template>
+    <v-card>
+        <v-card-title>
+            {{currentDate}}
+            <div class="flex-grow-1"></div>
+            <v-text-field v-model="search" append-icon="search" label="Поиск" single-line hide-details></v-text-field>
+        </v-card-title>
+        <v-container>
+            <v-row no-gutters>
+                <v-col v-for="team in teams" v-bind:key="team.title" cols="12" sm="4">
+                    <v-card class="ma-1" outlined tile>
+                        <v-data-table
+                                :headers="headers"
+                                :items="team.players"
+                                :search="search"
+                                :items-per-page="30"
+                                hide-default-footer
+                                fixed-header
+                                dark
+                        >
+                            <template v-slot:header="{ props: { headers }  }">
+                                <thead>
+                                <tr>
+                                    <th :colspan="headers.length-2">
+                                        <v-chip color="orange" label outlined>{{team.title}}</v-chip>
+                                    </th>
+                                    <th colspan="2">
+                                        <v-chip color="orange" label outlined>
+                                            {{team.total.toFixed(1)}}
+                                            <v-icon right>star</v-icon>
+                                        </v-chip>
+                                    </th>
+                                </tr>
+                                </thead>
+                            </template>
+                            <template v-slot:body.prepend="{headers}">
+                                <tr class="d-sm-none">
+                                    <th
+                                            :class="getClassForCol(header.value)"
+                                            v-for="header in headers"
+                                            v-bind:key="header.text"
+                                    >{{header.text}}
+                                    </th>
+                                </tr>
+                            </template>
+                            <template v-slot:item="{item}">
+                                <tr :style=" getColor(item.division)">
+                                    <td :class="getClassForCol('name')">
+                                        <router-link class="player__link" :to="translit(item.name)">{{item.name}}
+                                        </router-link>
+                                    </td>
+                                    <td :class="getClassForCol('games')">
+                                        <router-link class="player__link" :to="translit(item.name)">
+                                            {{item.games
+                                          ? item.games : 0 }}
+                                        </router-link>
+                                    </td>
+                                    <td :class="getClassForCol('games')">
+                                        <router-link class="player__link" :to="translit(item.name)">
+                                            {{item.unique_games ?
+                                          item.unique_games : 0}}
+                                        </router-link>
+                                    </td>
+                                    <td :class="getClassForCol('points')">
+                                        <router-link class="player__link" :to="translit(item.name)">
+                                            {{item.points ?
+                                          item.points : 0}}
+                                        </router-link>
+                                    </td>
+                                </tr>
+                            </template>
 
-              <template v-slot:footer>
-                <div class="table__footer pa-2 d-flex justify-space-between">
-                  <div class="table__footerName">
-                    <strong>Итого</strong>
-                  </div>
-                  <div class="table__footerValue">
-                    <v-chip color="orange" text-color="white">
-                      {{team.total.toFixed(1)}}
-                      <v-icon right>star</v-icon>
-                    </v-chip>
-                  </div>
-                </div>
-              </template>
-            </v-data-table>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+                            <template v-slot:footer>
+                                <div class="table__footer pa-2 d-flex justify-space-between">
+                                    <div class="table__footerName">
+                                        <strong>Итого</strong>
+                                    </div>
+                                    <div class="table__footerValue">
+                                        <v-chip color="orange" text-color="white">
+                                            {{team.total.toFixed(1)}}
+                                            <v-icon right>star</v-icon>
+                                        </v-chip>
+                                    </div>
+                                </div>
+                            </template>
+                        </v-data-table>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-card>
 </template>
 <script>
-export default {
-  name: "ligue",
-  props: {},
-  data() {
-    return {
-      currentDate: new Date().toLocaleString("ru", {
-        month: "long",
-        day: "numeric",
-        weekday: "long",
-        timezone: "UTC"
-      }),
-      search: "",
-      slots: ["header", "progress", "body.append", "body.prepend", "footer"],
-      headers: [
-        {
-          text: "Игрок ",
-          align: "left",
-          sortable: false,
-          value: "name"
-        },
-        { text: "Матчи", value: "games" },
-        { text: "Ун. матч", value: "unique_games" },
-        { text: "Очки", value: "points" }
-      ]
-    };
-  },
-  methods: {
-    getClassForCol: value => {
-      switch (value) {
-        case "name":
-          return "player__name";
-        case "games":
-          return "player__games";
-        case "unique_games":
-          return "player__games";
-        case "points":
-          return "player__points";
+  export default {
+    name: "ligue",
+    props: {},
+    data() {
+      return {
+        currentDate: new Date().toLocaleString("ru", {
+          month: "long",
+          day: "numeric",
+          weekday: "long",
+          timezone: "UTC"
+        }),
+        search: "",
+        slots: ["header", "progress", "body.append", "body.prepend", "footer"],
+        headers: [
+          {
+            text: "Игрок ",
+            align: "left",
+            sortable: false,
+            value: "name",
+          },
+          {text: "Матчи", value: "games", class: "pa-1"},
+          {text: "Уник.", value: "unique_games"},
+          {text: "Очки", value: "points"}
+        ]
+      };
+    },
+    methods: {
+      getClassForCol: value => {
+        switch (value) {
+          case "name":
+            return "player__name";
+          case "games":
+            return "player__games pa-1";
+          case "unique_games":
+            return "player__games pa-1";
+          case "points":
+            return "player__points pa-1";
+        }
       }
-    }
-  },
-  computed: {
-    teams() {
-      return this.$store.getters.players;
-    }
-  },
-  beforeRouteLeave(to, from, next) {
-    const playerName = to.params.playername;
-    /*find player in vuex*/
-    let detailPlayer = null;
-    this.$store.dispatch("setDetailPlayer", null);
-    for (const team in this.teams) {
-      detailPlayer = this.teams[team].players.filter(item => {
-        if (this.translit(item.name) === playerName) return item;
-      });
-      if (detailPlayer.length > 0) {
-        this.$store.dispatch("setDetailPlayer", detailPlayer);
-        break;
+    },
+    computed: {
+      teams() {
+        return this.$store.getters.players;
       }
+    },
+    beforeRouteLeave(to, from, next) {
+      const playerName = to.params.playername;
+      /*find player in vuex*/
+      let detailPlayer = null;
+      this.$store.dispatch("setDetailPlayer", null);
+      for (const team in this.teams) {
+        detailPlayer = this.teams[team].players.filter(item => {
+          if (this.translit(item.name) === playerName) return item;
+        });
+        if (detailPlayer.length > 0) {
+          this.$store.dispatch("setDetailPlayer", detailPlayer);
+          break;
+        }
+      }
+      next();
     }
-    next();
-  }
-};
+  };
 </script>
 
 <style scoped>
-.player__name,
-.player__games,
-.player__points {
-  padding: 0;
-}
-.header__mobile .player__name,
-.header__mobile  .player__games,
-.header__mobile .player__points 
-.header__mobile .player__name {
-  padding: 0 10px
-}
+    .player__name, .player__link {
+        text-align: left;
+    }
 
-.player__games {
-  width: 17%;
-}
-
-.player__points {
-  width: 20%;
-}
-
-.player__link {
-  display: block;
-  width: 100%;
-  color: inherit;
-  text-decoration-line: none;
-  padding: 0 10px;
-}
+    .player__games,
+    .player__points {
+        text-align: center;
+    }
+    
+    .player__link {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        color: inherit;
+        text-decoration-line: none;
+    }
+    .player__games .player__link, .player__points .player__link {
+        text-align: center;
+    }
+    @media all and (max-width: 340px) {
+        .player__link {
+            font-size: 11px;
+        }
+    }
 </style>
