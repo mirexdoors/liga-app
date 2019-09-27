02b531e485e9c0ac7ad9f63c5e88a3ac66d63f7f
mirexdoors <mirex.doors@mail.ru>
2019-09-27 12:07:42 +0700

    add points per game

--- src/components/player.vue
+++ src/components/player.vue
@@ -110,6 +110,7 @@
             {text: "Игры", value: player.games},
             {text: "Уникальные игры", value: player.unique_games},
             {text: "Очки", value: player.points},
+            {text: "Очков за игру ср.", value: (player.points/player.games).toFixed(2)},
             {text: "% от очков команды", value: getPercentTotal(player, this.teams), align: "left"}
           ];
         } else {
