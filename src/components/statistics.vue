5eab2d208dae1adc1b5bef6f261cf993259b0fbc
mirexdoors <mirex.doors@mail.ru>
2019-09-27 18:03:36 +0700

    start chart func

--- src/components/statistics.vue
+++ src/components/statistics.vue
@@ -1,11 +1,39 @@
 <template>
     <v-container class="d-flex flex-column align-center justify-center">
-    <h1 class="title">Статистика</h1>
-    <div class="caption">Coming soon ...</div>
+        <h1 class="title">Статистика</h1>
+        <div class="caption">Coming soon ...</div>
+        <line-chart
+                v-if="loaded"
+                :chartData="chartData"
+                :options="options"/>
     </v-container>
 </template>
 <script>
-    export default {
-      name: 'statistics'
+  import lineChart from './charts/lineChart.vue';
+
+  export default {
+    name: 'statistics',
+    components: {lineChart},
+    data: () => ({
+      loaded: false,
+      chartData: null,
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }),
+    async mounted() {
+      this.loaded = false;
+      const matchList = await fetch('http://mirexda2.beget.tech/get/games/all/')
+      .then(response => {
+        return response.json();
+      })
+      .then(matchesJSON => {
+        return matchesJSON.games;
+      });
+      this.chartData = matchList;
+      this.loaded = true;
+
     }
+  }
 </script>
