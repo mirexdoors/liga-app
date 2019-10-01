8b25732c9c7adca79922f21ae126e5dc4474b7c6
Mikhail <mirex.doors@mail.ru>
2019-10-02 02:13:50 +0700

    add main statistic

--- src/components/statisticComponents/mainStat.vue
+++ src/components/statisticComponents/mainStat.vue
@@ -10,7 +10,7 @@
                     >
                         Уникальные матчи
                     </v-card>
-                    <v-row no-gutters v-for="uniqItem in uniqGames">
+                    <v-row no-gutters v-for="uniqItem in uniqGames" v-bind:key="uniqItem.title">
                         <v-col
                                 cols="8"
                                 sm="8"
@@ -41,7 +41,7 @@
         </v-col>
         <v-col cols="12" sm="4">
             <v-row>
-                <v-col sm="9"  class="caption">
+                <v-col sm="9" class="caption">
                     <v-card
                             class="orange--text pa-1 align-center"
                             outlined
@@ -49,7 +49,7 @@
                     >
                         Все матчи
                     </v-card>
-                    <v-row no-gutters v-for="allGamesItem in allGames">
+                    <v-row no-gutters v-for="allGamesItem in allGames" v-bind:key="allGamesItem.title">
                         <v-col
                                 cols="8"
                                 sm="8"
@@ -67,7 +67,7 @@
                                 sm="4"
                         >
                             <v-card
-                                    class="pa-2"
+                                    class="pa-1"
                                     outlined
                                     tile
                             >
@@ -80,7 +80,7 @@
         </v-col>
         <v-col cols="12" sm="4">
             <v-row>
-                <v-col sm="9"  class="caption">
+                <v-col sm="9" class="caption">
                     <v-card
                             class="orange--text pa-1 align-center"
                             outlined
@@ -88,13 +88,13 @@
                     >
                         Время лиги
                     </v-card>
-                    <v-row no-gutters v-for="timeItem in ligueTime">
+                    <v-row no-gutters v-for="timeItem in ligueTime" v-bind:key="timeItem.title">
                         <v-col
                                 cols="8"
                                 sm="8"
                         >
                             <v-card
-                                    class="pa-2"
+                                    class="pa-1"
                                     outlined
                                     tile
                             >
@@ -106,7 +106,7 @@
                                 sm="4"
                         >
                             <v-card
-                                    class="pa-2"
+                                    class="pa-1"
                                     outlined
                                     tile
                             >
@@ -125,12 +125,12 @@
     name: "main-stat",
     data: () => ({
       uniqGames: {
-        gamesPlayed:{title: 'Сыграно матчей', value: null},
+        gamesPlayed: {title: 'Сыграно матчей', value: null},
         gamesLeft: {title: 'Осталось матчей', value: null},
         percentOfMax: {title: '% от максимума', value: null},
       },
       allGames: {
-        gamesPlayed:{title: 'Сыграно матчей', value: null},
+        gamesPlayed: {title: 'Сыграно матчей', value: null},
         gamesLeft: {title: 'Осталось матчей', value: null},
         percentOfMax: {title: '% от максимума', value: null},
       },
@@ -145,13 +145,33 @@
       this.ligueTime.daysPassed.value = getDaysPassed();
       this.ligueTime.daysLeft.value = getDaysLeft();
       this.ligueTime.percentOfAllTime.value = ((this.ligueTime.daysPassed.value / (this.ligueTime.daysLeft.value + this.ligueTime.daysPassed.value)) * 100).toFixed(1) + '%';
+      getGamesInfo(this.ligueData, this.allGames, this.uniqGames);
     },
     computed: {
-      teams() {
+      ligueData() {
         return this.$store.getters.players;
       }
     },
   }
+  const getGamesInfo = (data, allGames, uniqGames) => {
+    const ALL_POSSIBLE_MATCHES = 4356;
+    const ALL_POSSIBLE_MATCHES_UNIQ = 1452;
+    let totalMatches = 0;
+    let uniqMatches = 0;
+    for (let team in data) {
+      totalMatches += data[team]['total_games'];
+      uniqMatches += data[team]['total_unique_games'];
+    }
+
+    allGames.gamesPlayed.value = totalMatches / 2;
+    allGames.gamesLeft.value = ALL_POSSIBLE_MATCHES - totalMatches / 2;
+    allGames.percentOfMax.value = (((totalMatches / 2) / ALL_POSSIBLE_MATCHES) * 100).toFixed(2) + '%';
+    uniqGames.gamesPlayed.value = uniqMatches / 2;
+    uniqGames.gamesLeft.value = ALL_POSSIBLE_MATCHES_UNIQ - uniqMatches / 2;
+    uniqGames.percentOfMax.value = (((uniqMatches / 2) / ALL_POSSIBLE_MATCHES_UNIQ) * 100).toFixed(2) + '%';
+
+    return true;
+  };
   const getDaysPassed = () => {
     const d0 = new Date('September 01, 2019 00:00:00');
     const d1 = new Date();
