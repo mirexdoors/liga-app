7b30c4c9ef835fc48e8c0846f9b2bb5039f3c501
Mikhail <mirex.doors@mail.ru>
2019-10-05 02:42:52 +0700

    add jest, babel-jeat etc.

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "vue"],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    ".+\\.(css|styl|less|sass|scss|png|jpg|jpeg|mp3|ttf|woff|woff2)$": "jest-transform-stub",
+    "^.+\\.js?$": "babel-jest"
+  },
+  snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "<rootDir>/components/*.vue",
+    "<rootDir>/components/**/*.vue",
+  ]
+};
---
+++ src/tests/App.test.js
@@ -0,0 +1,8 @@
+import {shallowMount} from '@vue/test-utils';
+import App from '../App';
+
+describe ('test App', () => {
+  it ('works', ()=> {
+     shallowMount(App);
+  });
+});
