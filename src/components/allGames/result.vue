362ac6eec15521c60f7c766ba0950850a64755c0
mirexdoors <mirex.doors@mail.ru>
2019-11-10 15:12:46 +0700

    edit match function

--- src/components/allGames/result.vue
+++ src/components/allGames/result.vue
@@ -1,46 +1,51 @@
 <template>
-  <div>
-    <span v-if="!getAdmin">{{getScore}}</span>
-    <v-select v-else :items="permissibleScore" ref="val" v-model="getScore" label="Результат"></v-select>
-  </div>
+    <div>
+        <span v-if="!getAdmin">{{getScore}}</span>
+        <v-select v-else :items="permissibleScore" ref="val" v-model="getScore"
+                  label="Результат"></v-select>
+    </div>
 </template>
 
 <script>
-export default {
-  props: ["item"],
-  data() {
-    return {
-      permissibleScore: ["3-0", "3-1", "3-2", "2-3", "1-3", "0-3"]
-    };
-  },
-  computed: {
-    getAdmin() {
-      return this.$store.getters.getAdmin;
-    },
-    getScore: {
-      get() {
-        return this.item.score;
-      },
-      set(value) {
-        const newSocre = value.split("-")
-        const data = {
-          action: "edit",
-          id: this.item.id,
-          score_1: newSocre[0],
-          score_2: newSocre[1],
-          method: 'POST',
+    export default {
+        props: ["item"],
+        data() {
+            return {
+                permissibleScore: ["3-0", "3-1", "3-2", "2-3", "1-3", "0-3"]
+            };
+        },
+        computed: {
+            getAdmin() {
+                return this.$store.getters.getAdmin;
+            },
+            getScore: {
+                get() {
+                    return this.item.score;
+                },
+                set(value) {
+                    const newScore = value.split("-");
+                    const data = {
+                        action: "edit",
+                        id: this.item.id,
+                        date: this.item.date,
+                        score_1: newScore[0],
+                        score_2: newScore[1],
+                    };
+                    const formData = new FormData();
+                    for (const propName in data) {
+                        formData.append(propName, data[propName]);
+                    }
+                    const apiEditMatchScore = "http://league.sibsquash.ru/post/edit/";
+                    fetch(apiEditMatchScore, {
+                        method: 'POST',
+                        body: formData
+                    })
+                        .then(response => {
+                            return response.json();
+                        })
+                        .catch(error => console.log(error));
+                }
+            }
         }
-        const apiEditMatchScore = "http://mirexda2.beget.tech/post/edit/";
-        fetch(apiEditMatchScore, data)
-          .then(response => {
-            return response.json();
-          })
-          .then(response => {
-            console.log(response)
-          })
-          .catch(error => console.log(error));
-      }
-    }
-  }
-};
+    };
 </script>
