df8518376c91a96db9448598bb3abcd474719ecf
mirexdoors <mirex.doors@mail.ru>
2020-03-14 01:18:32 +0700

    add date input

--- src/components/addMatch.vue
+++ src/components/addMatch.vue
@@ -1,160 +1,206 @@
 <template>
-  <div>
-    <v-dialog v-model="dialogMatch" width="500">
-      <template v-slot:activator="{ on }">
-        <v-icon v-on="on">add</v-icon>
-        <v-icon v-on="on">sports_tennis</v-icon>
-      </template>
+    <div>
+        <v-dialog v-model="dialogMatch" width="500">
+            <template v-slot:activator="{ on }">
+                <v-icon v-on="on">add</v-icon>
+                <v-icon v-on="on">sports_tennis</v-icon>
+            </template>
 
-      <v-card>
-        <v-card-title class="headline grey" primary-title>Итоги матча</v-card-title>
-        <v-form>
-          <v-container>
-            <v-row>
-              <h3 class="mx-auto">Выберите игроков</h3>
-            </v-row>
-            <v-row>
-              <v-col cols="12" md="6">
-                <v-autocomplete
-                        label="Введите игрока"
-                        :items="playersOne"
-                        item-text="name"
-                        item-value="id"
-                        v-model="player1"
-                        filled
-                        rounded
-                ></v-autocomplete>
-              </v-col>
+            <v-card>
+                <v-card-title class="headline grey" primary-title>Итоги матча</v-card-title>
+                <v-form>
+                    <v-container>
+                        <v-row>
+                            <h3 class="mx-auto">Выберите игроков</h3>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12" md="6">
+                                <v-autocomplete
+                                        label="Введите игрока"
+                                        :items="playersOne"
+                                        item-text="name"
+                                        item-value="id"
+                                        v-model="player1"
+                                        filled
+                                        rounded
+                                ></v-autocomplete>
+                            </v-col>
 
-              <v-col cols="12" md="6">
-                <v-autocomplete
-                        label="Введите игрока"
-                        :items="playersTwo"
-                        item-text="name"
-                        item-value="id"
-                        v-model="player2"
-                        filled
-                        rounded
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
-            <v-row v-if="player1 && player2">
-              <h3 class="mx-auto">Счет</h3>
-            </v-row>
-            <v-row v-if="player1 && player2">
-              <v-col cols="12" md="12">
-                <v-select
-                  :items="permissibleScore"
-                  v-model="score"
-                  label="Результат"
-                ></v-select>
-              </v-col>
-            </v-row>
-            <v-row v-if="score">
-              <v-col cols="12" md="12">
-                <div class="text-center">
-                  <v-btn @click="getConfirm">Отправить</v-btn>
-                </div>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-form>
-      </v-card>
-    </v-dialog>
+                            <v-col cols="12" md="6">
+                                <v-autocomplete
+                                        label="Введите игрока"
+                                        :items="playersTwo"
+                                        item-text="name"
+                                        item-value="id"
+                                        v-model="player2"
+                                        filled
+                                        rounded
+                                ></v-autocomplete>
+                            </v-col>
+                        </v-row>
+                        <v-row v-if="player1 && player2">
+                            <h3 class="mx-auto">Счет</h3>
+                        </v-row>
+                        <v-row v-if="player1 && player2">
+                            <v-col cols="12" md="12">
+                                <v-select
+                                        :items="permissibleScore"
+                                        v-model="score"
+                                        label="Результат"
+                                ></v-select>
+                            </v-col>
+                        </v-row>
+                        <v-menu
+                                v-if="score"
+                                v-model="menu2"
+                                :close-on-content-click="false"
+                                transition="scale-transition"
+                                offset-y
+                                max-width="290px"
+                                min-width="290px"
+                        >
+                            <template v-slot:activator="{ on }">
+                                <v-text-field
+                                        v-model="computedDateFormatted"
+                                        persistent-hint
+                                        prepend-icon="event"
+                                        readonly
+                                        v-on="on"
+                                ></v-text-field>
+                            </template>
+                            <v-date-picker v-model="date" no-title @input="menu2 = false"></v-date-picker>
+                        </v-menu>
+                        <v-row v-if="score">
+                            <v-col cols="12" md="12">
+                                <div class="text-center">
+                                    <v-btn @click="getConfirm">Отправить</v-btn>
+                                </div>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-form>
+            </v-card>
+        </v-dialog>
 
-    <v-dialog v-model="dialogConfirm" width="500">
-      <v-card>
-        <v-container>
-          <v-card-text>Сохранить результат?</v-card-text>
-          <v-card-text class="headline mb-1">
-              {{getPlayerNameById(player1)}} - {{getPlayerNameById(player2)}}
-              <v-chip
-                      class="ma-2 headline mb-1"
-              >
-                  {{this.score}}
-              </v-chip>
-          </v-card-text>
-          <v-card-actions>
-            <div class="flex-grow-1"></div>
-            <v-btn color="red darken-1" @click="dialogConfirm = false">Отмена</v-btn>
-            <v-btn color="green darken-1" @click="saveResults">Сохранить</v-btn>
-          </v-card-actions>
-        </v-container>
-      </v-card>
-    </v-dialog>
-  </div>
+        <v-dialog v-model="dialogConfirm" width="500">
+            <v-card>
+                <v-container>
+                    <v-card-text>Сохранить результат?</v-card-text>
+                    <v-card-text class="headline mb-1">
+                        {{getPlayerNameById(player1)}} - {{getPlayerNameById(player2)}}
+                        <v-chip
+                                class="ma-2 headline mb-1"
+                        >
+                            {{this.score}}
+                        </v-chip>
+                    </v-card-text>
+                  <v-card-text>
+                    {{dateFormatted}}
+                  </v-card-text>
+                    <v-card-actions>
+                        <div class="flex-grow-1"></div>
+                        <v-btn color="red darken-1" @click="dialogConfirm = false">Отмена</v-btn>
+                        <v-btn color="green darken-1" @click="saveResults">Сохранить</v-btn>
+                    </v-card-actions>
+                </v-container>
+            </v-card>
+        </v-dialog>
+    </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      dialogMatch: false,
-      dialogConfirm: false,
-      player1: "",
-      player2: "",
-      score: "",
-      permissibleScore: ["3-0", "3-1", "3-2", "2-3", "1-3", "0-3"]
-    };
-  },
-  computed: {
-    playersOne() {
-      return this.getPlayerNames();
-    },
-    playersTwo() {
-      return this.getPlayerNames();
-    },
-  },
-  methods: {
-    getPlayerNameById(id) {
-      const players = this.getPlayerNames();
-      const player = players.filter(item => {
-        if (item.id === id) return true;
-      })[0];
-      if (player)
-        return player.name;
-    },
-    getPlayerNames() {
-      let players = [];
-      if (this.$store) {
-        const teamsFromStore = this.$store.getters.players;
-        for (let teamName in teamsFromStore) {
-          players = players.concat(teamsFromStore[teamName].players);
-        }
-      }
-      return players;
-    },
-    getConfirm() {
-      this.dialogMatch = false;
-      this.dialogConfirm = true;
-    },
-    saveResults() {
-      this.dialogConfirm = false;
-      if (this.player1 === this.player2) {
-        this.$emit("errorAuth", "Игроки должны различаться");
-        return;
-      }
-      const request = {
-        url: "http://league.sibsquash.ru/post/match/",
-        data: {
-          action: "add",
-          player_1: this.player1,
-          player_2: this.player2,
-          score_1: this.score[0],
-          score_2: this.score[2],
-        }
+  export default {
+    data() {
+      return {
+        dialogMatch: false,
+        dialogConfirm: false,
+        player1: "",
+        player2: "",
+        date: new Date().addHours(7).toISOString().substr(0, 10),
+        dateFormatted: this.formatDate(new Date().addHours(7).toISOString().substr(0, 10)),
+        menu2: false,
+        score: "",
+        permissibleScore: ["3-0", "3-1", "3-2", "2-3", "1-3", "0-3"]
       };
+    },
+    computed: {
+      playersOne() {
+        return this.getPlayerNames();
+      },
+      playersTwo() {
+        return this.getPlayerNames();
+      },
+      computedDateFormatted() {
+        return this.formatDate(this.date)
+      }
+    },
+    watch: {
+      date() {
+        this.dateFormatted = this.formatDate(this.date)
+      }
+    },
+    methods: {
+      formatDate(date) {
+        if (!date) return null;
+        const [year, month, day] = date.split('-');
+        return `${day}-${month}-${year}`
+      },
+      getPlayerNameById(id) {
+        const players = this.getPlayerNames();
+        const player = players.filter(item => {
+          if (item.id === id) return true;
+        })[0];
+        if (player)
+          return player.name;
+      },
+      getPlayerNames() {
+        let players = [];
+        if (this.$store) {
+          const teamsFromStore = this.$store.getters.players;
+          for (let teamName in teamsFromStore) {
+            players = players.concat(teamsFromStore[teamName].players);
+          }
+        }
+        return players;
+      },
+      getConfirm() {
+        this.dialogMatch = false;
+        this.dialogConfirm = true;
+      },
+      saveResults() {
+        this.dialogConfirm = false;
+        if (this.player1 === this.player2) {
+          this.$emit("errorAuth", "Игроки должны различаться");
+          return;
+        }
+        const request = {
+          url: "http://league.sibsquash.ru/post/match/",
+          data: {
+            action: "add",
+            player_1: this.player1,
+            player_2: this.player2,
+            score_1: this.score[0],
+            score_2: this.score[2],
+            date: this.dateFormatted,
+          }
+        };
 
-      this.$store.dispatch("addMatchResult", request)
-        .then(() => {
-          this.player1 = "";
-          this.player2 = "";
-          this.score = "";
-        })
-        .catch(error => {
-          this.$emit("errorAuth", error);
-        });
+        this.$store.dispatch("addMatchResult", request)
+            .then(() => {
+              this.player1 = "";
+              this.player2 = "";
+              this.score = "";
+              this.date = new Date().addHours(7).toISOString().substr(0, 10);
+            })
+            .catch(error => {
+              this.$emit("errorAuth", error);
+            });
+      }
     }
+  };
+
+  Date.prototype.addHours= function(h){
+    this.setHours(this.getHours()+h);
+    return this;
   }
-};
 </script>
